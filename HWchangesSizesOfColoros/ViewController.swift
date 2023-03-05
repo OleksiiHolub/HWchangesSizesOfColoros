417a5db2123220b20d31df0152d679aa1e996b5f
OleksiiHolub <holub.oleksii@outlook.com>
2023-03-05 21:22:59 +0200

    Initial Commit

---
+++ HWchangesSizesOfColoros/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWchangesSizesOfColoros
+//
+//  Created by Oleksii Holub on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
